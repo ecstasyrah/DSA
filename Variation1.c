5a8984a60ff0f2f606a9c9c29169e006e71f28e1
ecstasyrah_ <22102141@usc.edu.ph>
2025-08-29 14:40:39 +0800

    variaton 1 complete

--- Variation1.c
+++ Variation1.c
@@ -14,22 +14,13 @@ List deletePos(List L, int position);
 int locate(List L, int data);
 List insertSorted(List L, int data);
 void display(List L);
+void select(List L);
 
 int main(){
     List L = initialize(L);
-    int data = 0;
-    int position = 0;
 
-    display(L);
 
-    // printf("\nEnter Position to be inserted: ");
-    // scanf("%d", &position);
-    // printf("Enter Data to be inserted: ");
-    // scanf("%d", &data);
-    L = insertPos(L,8,2);
-    printf("\nEnter element you want to find: ");
-    scanf("%d", &data);
-    locate(L,data);
+    select(L);
 
     return 0;
 }
@@ -66,16 +57,108 @@ int locate(List L, int data){
 
 List insertPos(List L, int data, int position){
     int i;
-    if(L.count != MAX){
-        for(i = L.count-1; i!=position-1;i--){
-            L.elem[L.count-1] = L.elem[i];
+    for(i = L.count; i>=position - 1;i--){
+        if(position > L.count){
+            L.elem[L.count] = L.elem[i];
+        }else {
+            L.elem[i] = L.elem[i-1];
         }
-        }else if(L.count == MAX){
-            printf("\nYOU CAN'T ENTER IN A ANY POSITION ARRAY FULL\n Delete an element\n");
-        }
-        L.elem[i] = L.elem[position-1];
-            L.count++;
-    printf("\nNew List: \t");
+    }
+    L.elem[position - 1] = data;
+    L.count++;
+
+    printf("\nNew List: \n\t");
     display(L);
     return L;
+}
+
+List deletePos(List L, int position) {
+    int i;
+    for (i = position - 1; i <= L.count-1; i++) {
+        if(i == L.count-1){
+            L.count--;
+        }else {
+            L.elem[i] = L.elem[i+1];
+        }
+    }
+    printf("\nNew List: \n\t");
+    display(L);
+    return L;
+}
+
+List insertSorted(List L, int data) {
+    int pos = 0;
+    if (L.count != MAX) {
+        for (int i = 0; i < L.count;i++) {
+            if(L.elem[i] > data) {
+                pos = i+1;
+                break;
+            }
+        }
+        insertPos(L,data,pos);
+
+    }else if (L.count == MAX){
+        printf("\nNeed to delete a value first");
+        int position;
+        printf("\nWhat position do you want to delete? ");
+        scanf("%d", &position);
+        deletePos(L,position);
+        L.count--;
+        insertSorted(L,data);
+    }
+
+}
+
+void select(List L) {
+    int sel;
+    printf("\nChoose an option: \n 1. Insert Position\n 2. Delete an element\n 3. Locate an element\n 4. Insert Sorted\n 5. Display list\n 6. Exit\n Enter number: ");
+    scanf("%d", &sel);
+    int data, position;
+    switch (sel) {
+        case 1:
+            if(L.count != MAX){
+                printf("\nInsert Position: ");
+                printf("\nEnter Position to be inserted: ");
+                scanf("%d", &position);
+                printf("Enter Data to be inserted: ");
+                scanf("%d", &data);
+                L = insertPos(L,data, position);
+            }else {
+                printf("\nYOU CAN'T ENTER IN ANY POSITION !! ARRAY FULL\n Delete an element in position: ");
+                scanf("%d", &position);
+                L = deletePos(L,position);
+            }
+            select(L);
+            break;
+        case 2:
+            printf("Delete\n");
+            int pos;
+            printf("Delete an element in position: ");
+            scanf("%d", &pos);
+            L = deletePos(L, pos);
+            select(L);
+            break;
+        case 3:
+            printf("\nLocate data: ");
+            printf("\nEnter element you want to find: ");
+            scanf("%d", &data);
+             locate(L,data);
+            select(L);
+            break;
+        case 4:
+            printf("Insert sorted: ");
+            printf("\nEnter data you want to insert: ");
+            L = insertSorted(L, data);
+            select(L);
+            break;
+        case 5:
+            printf("Display list: \n");
+            display(L);
+            select(L);
+            break;
+        case 6:
+
+            break;
+    }
+
 }
